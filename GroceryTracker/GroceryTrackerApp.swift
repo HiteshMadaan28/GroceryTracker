d4ae19e84c8598f199c516a5296e86e085a6db0c
HiteshMadaan28 <hiteshmadaan28@gmail.com>
2025-04-02 18:18:55 +0530

    Initial Commit

---
+++ GroceryTracker/GroceryTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryTrackerApp.swift
+//  GroceryTracker
+//
+//  Created by Hitesh Madaan on 02/04/25.
+//
+
+import SwiftUI
+
+@main
+struct GroceryTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GroceryTrackerTests/GroceryTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryTrackerTests.swift
+//  GroceryTrackerTests
+//
+//  Created by Hitesh Madaan on 02/04/25.
+//
+
+import Testing
+@testable import GroceryTracker
+
+struct GroceryTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
